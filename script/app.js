7131138d6e1c398921b7fdb5d524b7184396e250
Mustafa Bharmal <mustafa.bharmal178@gmail.com>
2025-10-31 18:39:56 +0530

    perf(consent): defer Swiper and ad initialization until after consent

--- script/app.js
+++ script/app.js
@@ -5,7 +5,26 @@ const CONTENT_ARTICLE_TEASERS = [
   "This is an article about third party that happened this weekend" ];
 
 const closeBox = () => {
+  // Load Swiper stylesheet
+  document.head.insertAdjacentHTML('beforeend', `
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" media="print"
+    onload="this.media='all'">
+  <noscript>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+  </noscript>
+  `);
+
+  // Load Swiper script and initialize when ready
+  const swiperScript = document.createElement('script');
+  swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js';
+  swiperScript.async = true;
+  swiperScript.onload = () => {
+    initSwiper();
+  };
+  document.head.appendChild(swiperScript);
+
   initAd();
+  dynamicContent();
   document.body.classList.remove('no-scroll');
   document.querySelector('.cookieLayer__base').classList.add('cookieLayer__base--hidden');
 };
@@ -26,23 +45,3 @@ const dynamicContent = () => {
     teaser.innerHTML = CONTENT_ARTICLE_TEASERS[index];
   });
 };
-
-// Advertisement mock
-  const initAd = () => {
-    const adSpace = document.querySelector('.main__advertising');
-    const gif = '<div style="width:400px;max-width:100%;"><div style="height:0;padding-bottom:52.4%;position:relative;"><iframe width="500" height="262" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/6wy03z" title="Advertisement meme" loading="lazy" fetchpriority="low"></iframe></div></div>';
-    if ('requestIdleCallback' in window) {
-      requestIdleCallback(() => {
-        adSpace.innerHTML = gif;
-      });
-    } else {
-      setTimeout(() => adSpace.innerHTML = gif, 1000);
-    }
-  };
-
-
-const initApp = () => {
-  dynamicContent();
-};
-
-initApp();
